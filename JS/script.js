9e8f5914e824f1e9a0aaf0aa414b3bd82813ce4a
Srivikas Mathesvaran <93981440+Srivi24@users.noreply.github.com>
2024-10-13 22:33:50 +0530

    Top Bar Added

---
+++ JS/script.js
@@ -0,0 +1,30 @@
+const searchInput = document.getElementById('search-input');
+const searchIcon = document.getElementById('search-icon');
+const iconImg = document.getElementById('icon-img');
+
+searchInput.addEventListener('focus', () => {
+     if (searchInput.value == '') {
+        iconImg.style = "display: none";
+    }
+});
+
+searchInput.addEventListener('blur', () => {
+    if (searchInput.value === '') {
+        iconImg.style = "display: grid";
+    }
+});
+
+searchIcon.addEventListener('click', () => {
+    if (searchInput.value === '') {
+        searchInput.focus();
+    } 
+});
+
+searchInput.addEventListener('input', () => {
+    if (searchInput.value == '') {
+        iconImg.src = '../static/images/search.png';
+    }
+    else {
+        iconImg.style = "display: none";
+    }
+});
